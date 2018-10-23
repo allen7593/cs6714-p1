897e9147dcf0070e43197a1284cc0cae8429660c
Wendy <xiaowen.zhuang@student.unsw.edu.au>
2018-10-23 17:07:42 +1100

    Minor changes on todo.py and added several test case

---
+++ test.py
@@ -0,0 +1,169 @@
+import unittest
+
+from todo import evaluate, f1_score
+
+
+class EvaluationTest(unittest.TestCase):
+    def testEvaluation_1(self):
+        golden_list = [['B-TAR', 'I-TAR', 'O', 'B-HYP'], ['B-TAR', 'O', 'O', 'B-HYP']]
+        predict_list = [['B-TAR', 'O', 'O', 'O'], ['B-TAR', 'O', 'B-HYP', 'I-HYP']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, f1_score(1 / 4, 1 / 3))
+
+    def testEvaluation_2(self):
+        golden_list = [['B-TAR', 'I-TAR', 'O', 'B-HYP'], ['B-TAR', 'O', 'O', 'B-HYP']]
+        predict_list = [['O', 'O', 'O', 'O'], ['B-TAR', 'O', 'O', 'O']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, 0.4)
+
+    def testEvaluation_3(self):
+        golden_list = [['B-TAR', 'I-TAR', 'O', 'B-HYP'], ['B-TAR', 'O', 'O', 'B-HYP']]
+        predict_list = [['O', 'O', 'I-TAR', 'O'], ['B-TAR', 'O', 'O', 'O']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, 0.4)
+
+    def testEvaluation_4(self):
+        golden_list = [['B-TAR', 'I-TAR', 'O', 'B-HYP'], ['B-TAR', 'O', 'O', 'B-HYP']]
+        predict_list = [['O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, 0.0)
+
+    def testEvaluation_5(self):
+        golden_list = [
+            ['B-TAR', 'O', 'O', 'B-HYP', 'I-HYP', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'B-TAR', 'O', 'O', 'O', 'O', 'B-HYP', 'I-HYP', 'I-HYP', 'O',
+             'O', 'O', 'O', 'O', 'O', 'O', 'O']]
+        predict_list = [
+            ['B-TAR', 'O', 'O', 'O', 'B-HYP', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O'], [
+                'O', 'I-TAR', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'B-TAR', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+                'O',
+                'O', 'O', 'O', 'I-HYP', 'O']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, f1_score(2 / 4, 2 / 3))
+
+    def testEvaluation_6(self):
+        golden_list = [['B-TAR', 'I-TAR', 'B-TAR', 'B-HYP'], ['B-TAR', 'O', 'O', 'B-HYP']]
+        predict_list = [['B-TAR', 'I-TAR', 'I-TAR', 'O'], ['I-TAR', 'O', 'O', 'O']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, f1_score(1 / 5, 1 / 1))
+
+    def testEvaluation_7(self):
+        golden_list = [['B-TAR', 'I-TAR', 'I-TAR', 'B-HYP'], ['B-TAR', 'O', 'O', 'B-HYP']]
+        predict_list = [['B-TAR', 'B-TAR', 'I-TAR', 'O'], ['I-TAR', 'O', 'O', 'O']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, 0)
+
+
+    def testEvaluation_8(self):
+        golden_list = [['B-TAR', 'B-TAR', 'I-TAR', 'B-HYP', 'O', 'O'], ['B-TAR', 'O', 'O', 'B-HYP', '0', 'I-HYP']]
+        predict_list = [['B-TAR', 'B-TAR', 'I-TAR', 'O', 'B-TAR', 'I-TAR'], ['I-TAR', 'O', 'B-HYP', 'B-HYP', 'B-TAR']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, f1_score(3 / 6, 3 / 5))
+
+
+    # both lists don't have any results
+    def testEvaluation_9(self):
+        golden_list = [
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O', 'O', 'O', 'O', 'O', 'O', 'O']]
+        predict_list = [
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O', 'O', 'O', 'O', 'O', 'O', 'O']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, 1)
+
+
+    # random test case
+    def testEvaluation_10(self):
+        golden_list = [
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O', 'O', 'O', 'O', 'O', 'I-TAR', 'B-TAR']]
+        predict_list = [
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O', 'O', 'O', 'O', 'O', 'B-TAR', 'I-TAR']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, 0)
+
+    # no any relevent
+    # retrived some 
+    def testEvaluation_11(self):
+        golden_list = [
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O', 'O', 'O', 'O', 'O', 'O', 'O']]
+        predict_list = [
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O'],
+            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O', 'O', 'O', 'O', 'O', 'B-TAR', 'I-TAR']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, 0)
+
+    # sentence of different length
+    def testEvaluation_12(self):
+        golden_list  = [['B-TAR', 'I-TAR', 'I-TAR', 'B-HYP'], ['B-HYP','I-HYP','O','B-TAR','O'],['O', 'O', 'B-HYP']]
+        predict_list = [['O', 'O', 'O', 'O'], ['B-HYP','I-HYP','O','O','B-TAR'],['O', 'O', 'O']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, f1_score(1/2,1/5))
+
+    # no any retrived 
+    def testEvaluation_13(self):
+        golden_list = [['B-TAR', 'I-TAR', 'I-TAR', 'B-HYP'], ['B-TAR', 'O', 'O', 'B-HYP']]
+        predict_list = [['O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, 0)
+
+
+    # invalid lists
+    def testEvaluation_14(self):
+        golden_list = [['B-TAR', 'I-TAR', 'I-TAR', 'B-HYP'], ['O'],['B-TAR', 'I-TAR', 'O', 'B-HYP']]
+        predict_list = [['I-TAR', 'B-TAR', 'I-TAR', 'B-HYP'], ['B-TAR'],['I-TAR', 'O', 'O', 'O']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, f1_score(1/3,1/4))
+
+    # perfect match
+    def testEvaluation_15(self):
+        golden_list = [['B-TAR', 'I-TAR', 'I-TAR', 'B-HYP'], ['B-TAR', 'O', 'O', 'B-HYP']]
+        predict_list = [['B-TAR', 'I-TAR', 'I-TAR', 'B-HYP'], ['B-TAR', 'O', 'O', 'B-HYP']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, f1_score(4/4,4/4))
+
+    def testEvaluation_15(self):
+        golden_list = [['B-TAR', 'I-TAR', 'I-TAR', 'B-HYP'], ['B-TAR', 'O', 'O', 'B-HYP']]
+        predict_list = [['B-TAR', 'I-TAR', 'I-TAR', 'I-HYP'], ['B-TAR', 'O', 'O', 'B-HYP']]
+
+        f1 = evaluate(golden_list, predict_list)
+        self.assertEqual(f1, f1_score(3/3,3/4))
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- todo.py
+++ todo.py
@@ -1,24 +1,58 @@
 import torch
-import torch.nn.utils.rnn as rnn
 import torch.nn.functional as F
-from torch.nn._functions.thnn import rnnFusedPointwise as fusedBackend
+import torch.nn.utils.rnn as rnn
 from config import config
-
 _config = config()
 
+# At least up to 3 decimal places
+def evaluate(golden_list, predict_list):
+    golden_tags = get_tags(golden_list)
+    gt = get_dict_len(golden_tags)
+    predict_tags = get_tags(predict_list)
+    predict_len = get_dict_len(predict_tags)
+    f1 = 0
+    if (gt == 0) and (predict_len == 0):
+        f1 = 1.000
+    else:
+        try:
+            precision, recall = get_precision_recall(golden_tags, golden_list, predict_list, gt, predict_len)
+            f1 = f1_score(precision, recall)
+        except ZeroDivisionError:
+            f1 = 0.000
+    return f1
 
+'''
 def evaluate(golden_list, predict_list):
     try:
         precision, recall = get_precision_recall(golden_list, predict_list)
         return f1_score(precision, recall)
     except ZeroDivisionError:
         return 0
-
+'''
 
 def new_LSTMCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None):
-    # TODO:new_LSTMCell
-    pass;
 
+    if input.is_cuda:
+        igates = F.linear(input, w_ih)
+        hgates = F.linear(hidden[0], w_hh)
+        state = fusedBackend.LSTMFused()
+        return state(igates, hgates, hidden[1]) if b_ih is None else state(igates, hgates, hidden[1], b_ih, b_hh)
+
+    hx, cx = hidden
+    gates = F.linear(input, w_ih, b_ih) + F.linear(hx, w_hh, b_hh)
+    ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+
+    forgetgate = torch.sigmoid(forgetgate)
+    ingate_coupled = 1-forgetGate
+    cellgate = torch.tanh(cellgate)
+    outgate = torch.sigmoid(outgate)
+
+    cy = (forgetgate * cx) + (ingate_coupled * cellgate)
+    hy = outgate * torch.tanh(cy)
+
+    return hy, cy
+
+    pass;
 
 def get_char_sequence(model, batch_char_index_matrices, batch_word_len_lists):
     batch_size = batch_char_index_matrices.size()
@@ -34,6 +68,8 @@ def get_char_sequence(model, batch_char_index_matrices, batch_word_len_lists):
     output_sequence, state = model.char_lstm(output_sequence)
 
     output_sequence = torch.transpose(state[0], 0, 1).contiguous()
+    # output_sequence = torch.transpose(output_sequence, 0, 1).contiguous()
+    # output_sequence = torch.transpose(output_sequence, 1, 2).contiguous()
 
     tmp_size = output_sequence.size()
     output_sequence = output_sequence.view(batch_size[0] * batch_size[1], tmp_size[1] * tmp_size[2])
@@ -44,6 +80,36 @@ def get_char_sequence(model, batch_char_index_matrices, batch_word_len_lists):
     return output_sequence
 
 
+'''
+# Wendy's
+def get_char_sequence(model, batch_char_index_matrices, batch_word_len_lists):
+        
+    batch_size = batch_char_index_matrices.size() # [num_of_len, num_of_word, num_of_char]
+    minibatch = batch_char_index_matrices.view(batch_size[0] * batch_size[1], batch_size[2]) # [total_num_of_word, num_of_char]
+    minibatch_word_length_list = batch_word_len_lists.view(batch_size[0] * batch_size[1]) # [word_length]
+    
+    input_char_embeds = model.char_embeds(minibatch)    
+    perm_idx, sorted_minibatch_word_len_list = model.sort_input(minibatch_word_length_list)
+    sorted_input_embeds = input_char_embeds[perm_idx]
+    _, desorted_indices = torch.sort(perm_idx, descending=False)
+    output_sequence = rnn.pack_padded_sequence(sorted_input_embeds,
+                                               lengths=sorted_minibatch_word_len_list.data.tolist(), batch_first=True)
+
+    output_sequence, (hidden_state, cell_state) = model.char_lstm(output_sequence)
+    
+    # hidden_state: [num_of_layer*dir, batch_size, hidden_dim]
+    hidden_state = torch.cat((hidden_state[0,:,:],hidden_state[1,:,:]), -1)
+    hidden_state = hidden_state[desorted_indices]
+    result = hidden_state.view(batch_size[0], batch_size[1], -1)
+    return result
+'''
+
+
+def get_precision_recall(golden_tags, golden_list, predict_list, gt, predict_len) -> tuple:
+    tp = get_tp(golden_tags, golden_list, predict_list)
+    return tp / gt, tp / predict_len
+
+'''
 def get_precision_recall(golden_list, predict_list) -> tuple:
     golden_tags = get_tags(golden_list)
     gt = get_dict_len(golden_tags)
@@ -51,8 +117,11 @@ def get_precision_recall(golden_list, predict_list) -> tuple:
     predict_len = get_dict_len(predict_tags)
     tp = get_tp(golden_tags, golden_list, predict_list)
     return tp / gt, tp / predict_len
+'''
 
 
+# The number of tags(results) in the tags_dictionary
+# tags: {SentNum: [[wordNum, wordNum], [..]]}
 def get_dict_len(tags: dict):
     length = 0
     for key in tags:
@@ -64,6 +133,64 @@ def f1_score(precision, recall) -> float:
     return (2.0 * precision * recall) / (precision + recall)
 
 
+def get_tags(tag_list: list):
+    tar_find = False
+    hyp_find = False
+    current_tag = [] # [wordNum, wordNum]
+    tags_result = {} # {SentNum: [[wordNum, wordNum], ]}
+    for num_of_sent, sent in enumerate(tag_list):
+        for num_of_word, word in enumerate(sent):
+            # Check if there are any tags are done
+            # If tar_find
+            if tar_find:
+                if word != "I-TAR": # == "O" or "I-HYP" or "B-HYP" or "B-TAR"
+                    if num_of_sent not in tags_result:
+                        tags_result[num_of_sent] = [current_tag]
+                    else:
+                        tags_result[num_of_sent].append(current_tag)
+                    # reset the current tag and tar_find
+                    current_tag = list()
+                    tar_find = False
+                else:
+                    current_tag.append(num_of_word)
+            # If hy_find
+            elif hyp_find:
+                if word != "I-HYP": # == "O" or "I-TAR" or "B-TAR" or "B-HYP"
+                    if num_of_sent not in tags_result:
+                        tags_result[num_of_sent] = [current_tag]
+                    else:
+                        tags_result[num_of_sent].append(current_tag)
+                    # reset the current tag and hyp_find
+                    current_tag = list()
+                    hyp_find = False
+                else:
+                    current_tag.append(num_of_word)
+
+            # Deal with the current word
+            # Valid I-TAR and I-HYP are dealt with in the tag found case above
+            # Invalid I-* and O should be ignored(the invalid I-* shouldn't appear)
+            # B-TAR and B-HYP should be dealt with in both cases      
+            if word == "B-TAR":
+                tar_find = True
+                current_tag = list()
+                current_tag.append(num_of_word)
+            if word == "B-HYP":
+                hyp_find = True
+                current_tag = list()
+                current_tag.append(num_of_word)
+        # the last check on tar_find and hyp_find
+        if tar_find or hyp_find:
+            if num_of_sent not in tags_result:
+                tags_result[num_of_sent] = [current_tag]
+            else:
+                tags_result[num_of_sent].append(current_tag)
+        tar_find = False
+        hyp_find = False
+        current_tag = list()
+
+    return tags_result
+
+'''
 def get_tags(tag_list: list):
     tar_find = False
     hyp_find = False
@@ -112,8 +239,11 @@ def get_tags(tag_list: list):
         hyp_find = False
         current_tag = list()
     return tags_result
+'''
 
 
+
+'''
 def finalised_list(num_of_word, len_of_sent, num_of_sent, tags_result, current_tag, match):
     if num_of_word == len_of_sent - 1:
         if num_of_sent not in tags_result:
@@ -122,21 +252,24 @@ def finalised_list(num_of_word, len_of_sent, num_of_sent, tags_result, current_t
             tags_result[num_of_sent].append(current_tag)
         current_tag = list()
         match = False
+'''
 
 
+# get the true positive value derived from golden_list and predict_list
+# golden_tags: {SentNum: [[wordNum, wordNum], [..]]}
 def get_tp(golden_tags, golden_list, predict_list):
     match = 0
     for key in golden_tags:
-        sent_num = key
-        golden_tag = golden_tags[sent_num]
-        for tags in golden_tag:
+        sent_num = key # index of the sentence
+        golden_tag = golden_tags[sent_num] # tags_list in this sentence
+        for tags in golden_tag: # each tag in this sentence, like [wordNum, wordNum]
             if match_list(sent_num, tags, golden_list, predict_list):
                 match += 1
     return match
 
-
-def match_list(send_num: int, taget: list, golden_list, predict_list) -> bool:
+# Check if elements match given sentNum and wordNum
+def match_list(sent_num: int, taget: list, golden_list, predict_list) -> bool:
     for index in taget:
-        if golden_list[send_num][index] != predict_list[send_num][index]:
+        if golden_list[sent_num][index] != predict_list[sent_num][index]:
             return False
     return True
